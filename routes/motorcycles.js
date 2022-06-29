5feef4f029810f555caad8672fa464f5d5298b4a
alexandreahom <lexhom24@gmail.com>
2022-06-29 15:18:11 -0400

    Fix some CSS

--- routes/motorcycles.js
+++ routes/motorcycles.js
@@ -5,16 +5,11 @@ import { isLoggedIn } from '../middleware/middleware.js'
 const router = Router()
 
 router.get('/', motoCtrl.index)
-
 router.get('/new', motoCtrl.new)
-
 router.get('/:id', motoCtrl.show)
-
 router.get('/:id/edit', isLoggedIn, motoCtrl.edit)
-
 router.post('/', isLoggedIn, motoCtrl.create)
 router.post('/:id/reviews', motoCtrl.createReview)
-
 router.delete('/:id', motoCtrl.delete)
 
 export {
